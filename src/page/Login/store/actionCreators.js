4239ecd2761857e8ef88032a84f207b680bdde3f
1174716220@qq.com <vae658979>
2018-10-03 18:05:40 +0800

    登录特效加载

--- src/page/Login/store/actionCreators.js
+++ src/page/Login/store/actionCreators.js
@@ -2,7 +2,8 @@ import * as ActionConstants from './constants';
 import * as StringConstants from './../../../constant';
 import  axios from 'axios';
 import  {Modal} from 'antd';
-
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 //用户登录Action
 const  authLoginAction =  (loginToken)=>({
    type: ActionConstants.AUTH_LOGIN_ACTION,
@@ -11,21 +12,19 @@ const  authLoginAction =  (loginToken)=>({
 
 //用户登录
 export const authLogin = (values) => {
-    let _this = this;
+
     return (dispatch) => {
+        NProgress.start();
         //登录请求
         axios.post(StringConstants.SERVER_URL+"/sysUser/authLogin",JSON.stringify(values),
             {headers: {'Content-Type': 'application/json;charset=utf-8'}}).then((res)=>{
             if(res.status === 200){ //请求成功
                 let resp = res.data;
                 if(resp.resultCode === StringConstants.SERVER_SUCCESS_RES_CODE){
-                    Modal.success({
-                        title: '成功',
-                        content: "登录成功"
-                    });
                     //将token存入本地
                     localStorage.setItem("token",resp.data.token);
                     dispatch(authLoginAction(resp.data.token));
+                    NProgress.done();
                     //跳转到登录页
                     window.location.href = "/admin";
                 }else if(resp.resultCode === StringConstants.SERVER_FAIL_RES_CODE){
@@ -41,7 +40,7 @@ export const authLogin = (values) => {
                     content: "网络异常"
                 });
             }
-
+            NProgress.done();
         }).catch((e)=>{
             Modal.error({
                 title: '错误提示',
