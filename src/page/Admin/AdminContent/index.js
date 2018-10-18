c598f756af1a9cb2176e53e6cde649cfc92e9daa
yangjie <1174716220@qq.com>
2018-10-18 17:18:49 +0800

    后台首页面包屑导航添加

--- src/page/Admin/AdminContent/index.js
+++ src/page/Admin/AdminContent/index.js
@@ -1,7 +1,7 @@
 import React, {PureComponent} from 'react';
 import {connect} from 'react-redux';
-import {Layout} from 'antd';
-
+import {Layout, Breadcrumb} from 'antd';
+import './index.css';
 
 const {Content} = Layout;
 
@@ -10,8 +10,12 @@ class AdminContent extends PureComponent {
 
     render() {
         return (
-            <Content style={{margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280}}>
-                Content
+            <Content className='admin-layout-content'>
+                <Breadcrumb className="admin-content-bread">
+                    <Breadcrumb.Item>首页</Breadcrumb.Item>
+                    <Breadcrumb.Item>表单</Breadcrumb.Item>
+                </Breadcrumb>
+                <div className="admin-normal-content">Content</div>
             </Content>
         )
     }
