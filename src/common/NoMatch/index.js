42ccdeed85468b386d492b6ad51a2e8ce897be19
yangjie <1174716220@qq.com>
2018-10-29 14:47:37 +0800

    美化Footer

--- src/common/NoMatch/index.js
+++ src/common/NoMatch/index.js
@@ -16,36 +16,6 @@ class NoMatch extends Component{
                         <li>我们的服务器被外星人劫持了</li>
                     </ul>
                 </div>
-                <div className="img-404">
-                </div>
-                <p className="notfound-p">哎呀迷路了...</p>
-                <div className="notfound-reason">
-                    <p>可能的原因：</p>
-                    <ul>
-                        <li>原来的页面不存在了</li>
-                        <li>我们的服务器被外星人劫持了</li>
-                    </ul>
-                </div>
-                <div className="img-404">
-                </div>
-                <p className="notfound-p">哎呀迷路了...</p>
-                <div className="notfound-reason">
-                    <p>可能的原因：</p>
-                    <ul>
-                        <li>原来的页面不存在了</li>
-                        <li>我们的服务器被外星人劫持了</li>
-                    </ul>
-                </div>
-                <div className="img-404">
-                </div>
-                <p className="notfound-p">哎呀迷路了...</p>
-                <div className="notfound-reason">
-                    <p>可能的原因：</p>
-                    <ul>
-                        <li>原来的页面不存在了</li>
-                        <li>我们的服务器被外星人劫持了</li>
-                    </ul>
-                </div>
             </div>
 
         )
