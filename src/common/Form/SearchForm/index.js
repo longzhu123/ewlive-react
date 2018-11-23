79a52b0980f60c09545cde7e8c64d5879b5cb55b
yangjie <1174716220@qq.com>
2018-11-23 16:10:19 +0800

    SearchForm表单封装

---
+++ src/common/Form/SearchForm/index.js
@@ -0,0 +1,132 @@
+import React, {Component} from 'react';
+import {Select, Form, Radio, Checkbox, Button, Input, DatePicker} from "antd";
+import util from '../../../util/util';
+import './index.css';
+import moment from "moment"
+import 'moment/locale/zh-cn';
+
+
+moment.locale('zh-cn');
+const FormItem = Form.Item;
+const Option = Select.Option;
+const {RangePicker} = DatePicker;
+
+//公共的搜索表单组件
+class SearchForm extends Component {
+
+
+
+    //搜索表单提交事件
+    commit = ()=>{
+        const {getFieldsValue}=this.props.form;
+        let date1=getFieldsValue();
+        date1.isStart =  date1.isStart["_i"]; // 把向后段传值的moment的对象改为其_i属性；
+        this.props.formSubmit(date1);//最终 向后端输出；
+    };
+
+    //重置方法
+    reset = () => {
+        this.props.form.resetFields();
+        this.props.resetLoadGrid();
+    };
+
+    /**
+     * 初始化搜索表单列表
+     *
+     * 以这样的数据格式作为参数传递
+     *      {
+                type: "select",
+                lable: "机型",
+                initialValue: "1",
+                placeholder: "请选择您的机型",
+                width: "200px",
+                field:"iphone",
+                list: [{id:"1",name:"IphoneX"}, {id:"2",name:"Ihonexs"},{id:"3",name:"IhoneXsMax"}]
+            }
+     * @returns {Array}
+     */
+    initSearchFormList = () => {
+
+        const {getFieldDecorator} = this.props.form;
+        const formList = this.props.searchOptions;
+        let formItemList = [];
+
+
+        if (formList && formList.length > 0) {
+            formList.map((item, index) => {
+
+                let initialValue = item.initialValue || "";  // 默认为空
+                let lable = item.lable;           //标题
+                let placeholder = item.placeholder;  //placeholder
+                let width = item.width;   //width
+                let list = item.list || [];    //option
+                let field = item.field;   // 字段key
+
+                if (item.type === "input") {
+                    const input = <FormItem label={lable} key={field}>
+                        {getFieldDecorator(field, {initialValue: initialValue})(
+                            <Input placeholder={placeholder}/>
+                        )}
+                    </FormItem>
+                    formItemList.push(input);
+                }
+                else if (item.type === "select") {
+                    const select = <FormItem label={lable} key={field}>
+                        {getFieldDecorator(field, {
+                                initialValue: initialValue
+                            }
+                        )(
+                            <Select style={{width: width}} placeholder={placeholder}>
+                                {util.OptionList(list)}
+                            </Select>
+                        )}
+                    </FormItem>;
+                    formItemList.push(select);
+                } else if (item.type === "checkbox") {
+                    const checkbox = <FormItem label={lable} key={field}>
+                        {getFieldDecorator(field, {
+                            valuePropName: "checked",
+                            initialValue: initialValue,
+                        })(
+                            <Checkbox>{lable}</Checkbox>
+                        )}
+
+                    </FormItem>
+                    formItemList.push(checkbox);
+                } else if (item.type === "date") {
+                    const dateComponent = <FormItem label={lable} key={field}>
+                        {getFieldDecorator(field, {
+                            initialValue: moment(initialValue, 'YYYY/MM/DD')
+
+                        })(
+                            <DatePicker
+                                showTime
+                                for-mat="YY-MM-DD HH:mm:ss"
+                                placeholder={placeholder}
+                            />
+                        )}
+
+                    </FormItem>
+                    formItemList.push(dateComponent);
+
+                }
+            })
+
+        }
+
+        return formItemList;
+    };
+
+    render() {
+        return (<Form layout="inline">
+            {this.initSearchFormList()}
+            <FormItem>
+                <Button type="primary" onClick={this.commit()}>查询</Button>
+                <Button onClick={this.reset()}>重置</Button>
+            </FormItem>
+        </Form>)
+    }
+
+}
+
+export default Form.create()(SearchForm);
--- src/util/util.js
+++ src/util/util.js
@@ -1,13 +1,16 @@
-import axios from 'axios';
-import {Modal} from 'antd';
 import * as StringConstant from '../constant';
+import React from "react";
+import axios from 'axios';
+import {Modal, Select} from 'antd';
+
+const Option = Select.Option;
 
 let util = {
     //通用Ajax
     ajax(options) {
         options.data.token = localStorage.getItem("token");
         return new Promise((resolve, reject) => {
-            axios.post( options.url, JSON.stringify(options.data),
+            axios.post(options.url, JSON.stringify(options.data),
                 {headers: {'Content-Type': 'application/json;charset=utf-8'}}).then((response) => {
                 if (response.status === 200) {
                     let res = response.data;
@@ -39,30 +42,40 @@ let util = {
         });
     },
     //验证token是否有效
-    authToken(){
-        axios.post(StringConstant.SERVER_URL+"/sysUser/validateToken",JSON.stringify({"token":localStorage.getItem("token")}),
-            {headers: {'Content-Type': 'application/json;charset=utf-8'}}).then((res)=>{
-            if(res.status === 200){ //请求成功
+    authToken() {
+        axios.post(StringConstant.SERVER_URL + "/sysUser/validateToken", JSON.stringify({"token": localStorage.getItem("token")}),
+            {headers: {'Content-Type': 'application/json;charset=utf-8'}}).then((res) => {
+            if (res.status === 200) { //请求成功
                 let resp = res.data;
-                if(resp.resultCode === StringConstant.SERVER_SUCCESS_RES_CODE){
-                }else{
+                if (resp.resultCode === StringConstant.SERVER_SUCCESS_RES_CODE) {
+                } else {
                     window.location = '/login';
                 }
 
-            }else{
+            } else {
                 Modal.error({
                     title: '错误提示',
                     content: "网络异常"
                 });
             }
-        }).catch((e)=>{
+        }).catch((e) => {
             Modal.error({
                 title: '错误提示',
                 content: "网络异常"
             });
         });
+    },
+    //渲染下拉框列表
+    OptionList: (data) => {
+        if (!data) {
+            return [];
+        }
+        let OptionMap = [<Option value="0" key="all_key">全部</Option>];
+        data.map((item, index) => {
+            OptionMap.push(<Option value={item.id} key={item.id}>{item.name}</Option>)
+        });
+        return OptionMap;
     }
-
 };
 
 export default util;
