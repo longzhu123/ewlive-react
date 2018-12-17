23d7502c3ac68448cca592d2c7f1b16c3534d800
yangjie <1174716220@qq.com>
2018-12-17 10:12:44 +0800

    工具类的添加对象数组转换属性数组的方法

--- src/util/util.js
+++ src/util/util.js
@@ -76,6 +76,18 @@ let util = {
             return index;
         });
         return OptionMap;
+    },
+    /**
+     * 将对象数组里面,指定的属性拼接成一个list
+     * @param soureArray  对象数组
+     * @param filedName	指定的属性名
+     */
+    convertArrayToFiledList:(soureArray,filedName)=>{
+        let dataArray = [];
+        for(let item of soureArray){
+            dataArray.push(item[filedName]);
+        }
+        return dataArray;
     }
 };
 
